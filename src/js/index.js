8ff5737dfb29b1cdf30060af677d3faf4548c09b
kikeestrada <kikeestradadev@gmail.com>
2025-05-20 15:52:46 -0600

    adjustments

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import internalModule from './modules/internalModule';
+// import deleteUnbounceReset from './unbounceFeatures/deleteUnbounceReset'
+// import insertMetaTags from './unbounceFeatures/insertMetaTags'
+// import unbounceCookie from './unbounceFeatures/unbounceCookie'
+
+
 import Prism from 'prismjs';
 (() => {
 	internalModule();
+	// deleteUnbounceReset();
+	// insertMetaTags();
+	// unbounceCookie();
 	Prism.highlightAll(); // Corrected: Use Prism.highlightAll() instead of undefined prismjs()
 })();
